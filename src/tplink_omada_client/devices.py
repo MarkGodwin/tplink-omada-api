993929e8d99d66fcac874b4628c1751c4b94203c
Mike Heath <elcapo@gmail.com>
2023-03-15 23:59:56 +0000

    Fix missing key exception when device doesn't have 'uplink'

--- src/tplink_omada_client/devices.py
+++ src/tplink_omada_client/devices.py
@@ -238,6 +238,8 @@ class OmadaSwitch(OmadaDevice):
     @property
     def uplink(self) -> Optional[OmadaUplink]:
         """ Uplink device for this switch. """
+        if not "uplink" in self._data:
+            return None
         uplink = self._data["uplink"]
         if uplink is None:
             return None
