21a6e49e3859f7be228f1d449e5f8a020e24aa43
Mike Heath <heathmi@vmware.com>
2023-03-15 21:09:19 +0000

    Initial CLI

--- src/tplink_omada_client/cli/__init__.py
+++ src/tplink_omada_client/cli/__init__.py
@@ -1,99 +1,46 @@
 """ Omada CLI """
 import argparse
 import asyncio
+import sys
+from typing import Any, Sequence, Union
 
-from .config import ControllerConfig, to_omada_connection, set_controller_config
+from tplink_omada_client.exceptions import LoginFailed
 
-CONTROLLER_ARG: str = "controller"
+from . import (
+    command_devices,
+    command_target,
+    command_targets,
+)
 
-async def command_controllers(args):
-    """Executes 'controllers' subcommand"""
-    print("Controllers: ", args)
-
-
-async def command_devices(args):
-    """Executes 'devices' subcommand"""
-    print("Devices: ", args)
-
-
-async def command_set_controller(args):
-    """Executes 'set-controller' subcommand"""
-    if CONTROLLER_ARG not in args or args[CONTROLLER_ARG] is None:
-        raise argparse.ArgumentError(None, f"error: Missing --{CONTROLLER_ARG} argument")
-    controller = args[CONTROLLER_ARG]
-    # TODO Prompt if password empty
-    config = ControllerConfig(
-        url=args['url'],
-        username=args['username'],
-        password=args['password'],
-        site=args['site'],
-    )
-    # Connect to controller to validate config
-    async with to_omada_connection(config) as client:
-        await client.get_controller_name()
-        # TODO add simple site info API and call to validate site
-
-    set_controller_config(controller, config)
-
-
-def main():
+def main(argv: Union[Sequence[str], None] = None) -> int:
     """Entry point for Omada CLI"""
+    if argv is None:
+        argv = sys.argv[1:]
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-c', '--controller',
+        '-t', '--target',
         help="The target Omada controller",
-        )
+    )
 
     subparsers = parser.add_subparsers(
         title='Available commands',
         metavar='command',
-        dest="command",
-        )
-
-    # set-controller
-    set_parser = subparsers.add_parser(
-        "set-controller",
-        help="Set the Omada controller to use",
-        )
-    set_parser.set_defaults(func=command_set_controller)
-    set_parser.add_argument(
-        '--url',
-        help="The URL of the Omada controller",
-        required=True,
-        )
-    set_parser.add_argument(
-        '--username',
-        help="The name of the user used to authenticate",
-        required=True,
-        )
-    set_parser.add_argument(
-        '--password',
-        help="The user's password, password will be prompted if not provided",
-        )
-    set_parser.add_argument(
-        '--site',
-        help="The Omada site to user",
-        default = "Default",
     )
 
-    # controllers
-    controllers_parser = subparsers.add_parser(
-        "controllers",
-        help="Lists the controllers known to omada")
-    controllers_parser.set_defaults(func=command_controllers)
+    command_devices.arg_parser(subparsers)
+    command_target.arg_parser(subparsers)
+    command_targets.arg_parser(subparsers)
 
-    devices_parser = subparsers.add_parser(
-        "devices",
-        help="Lists devices known to Omada Controller")
-    devices_parser.set_defaults(func=command_devices)
-
-    args = parser.parse_args()
     try:
-        asyncio.run(args.func(vars(args)))
+        args = parser.parse_args(args=argv)
+        if "func" in args:
+            return asyncio.run(args.func(vars(args)))
+        parser.print_help()
+        return 1
     except argparse.ArgumentError as error:
         parser.print_usage()
         print(error.message)
-
-
-if __name__ == '__main__':
-    main()
+        return 1
+    except LoginFailed as error:
+        print(error)
+        return 2
---
+++ src/tplink_omada_client/cli/command_targets.py
@@ -0,0 +1,22 @@
+"""Implementation of 'targets' command"""
+from argparse import _SubParsersAction
+
+from .config import (
+    get_targets,
+)
+
+async def command_targets(args) -> int: # pylint: disable=unused-argument
+    """Executes 'targets' command"""
+    controllers = get_targets()
+    for (controller, url) in controllers:
+        print(f"{controller:15} {url}")
+    return 0
+
+def arg_parser(subparsers: _SubParsersAction) -> None:
+    """Configures argument parser for 'targets' command"""
+    # targets
+    controllers_parser = subparsers.add_parser(
+        "targets",
+        aliases=['t'],
+        help="Lists the configured targets")
+    controllers_parser.set_defaults(func=command_targets)
